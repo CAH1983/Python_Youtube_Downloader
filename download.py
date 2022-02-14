aeabb1a0777e57374e2f2f17c5824b22dba14202
Caroline Ho <carolineho1983@gmail.com>
2022-02-14 22:18:35 +0000

    added path label and button

--- download.py
+++ download.py
@@ -12,7 +12,7 @@ logo_img = PhotoImage(file='./Images/YouTube-logo-PNG-HD-1024x928.png')
 # resize
 logo_img = logo_img.subsample(3, 3)
 
-canvas.create_image(255, 80, image=logo_img)
+canvas.create_image(255, 100, image=logo_img)
 
 # configure the label and input
 link_field = Entry(screen, width=50)
@@ -20,10 +20,17 @@ link_label = Label(
     screen, text='Enter / Paste link to download: ', font=('Arial', 15))
 
 # Add label and input to window
-canvas.create_window(250, 250, window=link_label)
-canvas.create_window(250, 280, window=link_field)
+canvas.create_window(250, 260, window=link_label)
+canvas.create_window(250, 290, window=link_field)
+
+# Select path for where to save the file on the computer
+path_label = Label(screen, text='Select path for download', font=('Arial', 13))
+select_btn = Button(screen, text='Select')
+
+canvas.create_window(250, 320, window=path_label)
+canvas.create_window(250, 350, window=select_btn)
 
 download_btn = Button(screen, text="Download File")
 
-canvas.create_window(250, 330, window=download_btn)
+canvas.create_window(250, 400, window=download_btn)
 screen.mainloop()
