3136271568f02b5edf665dcd0c9e1776b1622347
Caroline Ho <carolineho1983@gmail.com>
2022-02-14 22:05:58 +0000

    fixed styling and added download button

--- download.py
+++ download.py
@@ -14,14 +14,16 @@ logo_img = logo_img.subsample(3, 3)
 
 canvas.create_image(255, 80, image=logo_img)
 
-# link field
+# configure the label and input
 link_field = Entry(screen, width=50)
 link_label = Label(
     screen, text='Enter / Paste link to download: ', font=('Arial', 15))
 
+# Add label and input to window
+canvas.create_window(250, 250, window=link_label)
+canvas.create_window(250, 280, window=link_field)
 
-# Add widgets to window
-canvas.create_window(250, 170, window=link_label)
-canvas.create_window(250, 220, window=link_field)
+download_btn = Button(screen, text="Download File")
 
+canvas.create_window(250, 330, window=download_btn)
 screen.mainloop()
