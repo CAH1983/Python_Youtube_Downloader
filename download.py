77b2d4368373046b05ec54038c58b55e80283a57
Caroline Ho <carolineho1983@gmail.com>
2022-02-14 21:50:48 +0000

    changed styling and added label plus input

--- download.py
+++ download.py
@@ -6,12 +6,22 @@ title = screen.title('Youtube downloader')
 canvas = Canvas(screen, width=500, height=500)
 canvas.pack()
 
-#image logo
+# image logo
 logo_img = PhotoImage(file='./Images/YouTube-logo-PNG-HD-1024x928.png')
 
 # resize
-logo_img = logo_img.subsample(2, 2)
+logo_img = logo_img.subsample(3, 3)
 
 canvas.create_image(255, 80, image=logo_img)
+
+# link field
+link_field = Entry(screen, width=50)
+link_label = Label(
+    screen, text='Enter / Paste link to download: ', font=('Arial', 15))
+
+
+# Add widgets to window
+canvas.create_window(250, 170, window=link_label)
+canvas.create_window(250, 220, window=link_field)
 
 screen.mainloop()
