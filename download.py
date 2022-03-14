bcff035457a8e7500cd20a4ffbeaf5e2cb109345
Caroline Ho <carolineho1983@gmail.com>
2022-03-14 19:35:56 +0000

    finished full playlist download feature

--- download.py
+++ download.py
@@ -3,7 +3,10 @@ from tkinter import filedialog
 from moviepy import *
 from moviepy.editor import VideoFileClip
 from pytube import YouTube
+import subprocess
+from pytube import Playlist
 import shutil
+import ffmpy
 
 # functions
 
@@ -28,10 +31,25 @@ def download_file():
     screen.title('Download complete! Download another file ..?')
 
 
-def download_playlist():
+# download whole playlist in MP3 audio
+def download_playlist_MP3_only():
+    print('downloading playlist clicked')
+    screen.title('started downloading playlist ...')
+    playlist_link = link_field.get()
     # get user path
-    get_playlist_link = playlist_input.get()
-    user_path = path_label.cget('text')
+    user_directory = path_label.cget('text')
+    playlist = Playlist(playlist_link)
+    for video in playlist.videos:
+        print('Downloading: {} with url: {}'.format(
+            video.title, video.watch_url))
+        screen.title('downloading ...')
+        video.streams.\
+            filter(type='video', progressive=True, file_extension='mp4').\
+            order_by('resolution').\
+            desc().\
+            first().\
+            download(user_directory)
+    print('finished downloading!')
 
 
 # styling
@@ -50,35 +68,29 @@ logo_img = logo_img.subsample(3, 3)
 
 canvas.create_image(255, 180, image=logo_img)
 
-# configure the labels and URL inputs for single download
+# configure the label and URL input for  download
 link_label = Label(
     screen, text='Enter / Paste link to download: ', font=('Arial', 15), bg=background_color, fg=font_color)
 link_field = Entry(screen, width=50)
 
-# configure the labels and URL inputs for multi download (playlist)
-playlist_label = Label(screen, text='Enter/Paste link of the playlist here',
-                       font=('Arial', 15), bg=background_color, fg=font_color)
-playlist_input = Entry(screen, width=50)
-
 
 # Add label and input to window
-canvas.create_window(250, 360, window=link_label)
-canvas.create_window(250, 390, window=link_field)
-canvas.create_window(250, 420, window=playlist_label)
-canvas.create_window(250, 450, window=playlist_input)
+canvas.create_window(250, 380, window=link_label)
+canvas.create_window(250, 420, window=link_field)
 
 # Select path for where to save the file on the computer
 path_label = Label(screen, text='Select path for download', font=(
     'Arial', 13),  bg=background_color, fg=font_color)
 select_btn = Button(screen, text='Select', command=select_path)
 
-canvas.create_window(250, 480, window=path_label)
-canvas.create_window(250, 510, window=select_btn)
+canvas.create_window(250, 460, window=path_label)
+canvas.create_window(250, 490, window=select_btn)
 
 download_btn = Button(screen, text="Download File",
                       command=download_file, bg='#cae9ff')
 
-download_playlist_btn = Button(screen, text="Download Playlist", bg='#ffd5c2')
+download_playlist_btn = Button(
+    screen, text="Download Playlist", bg='#ffd5c2', command=download_playlist_MP3_only)
 
 canvas.create_window(250, 540, window=download_btn)
 canvas.create_window(250, 570, window=download_playlist_btn)
